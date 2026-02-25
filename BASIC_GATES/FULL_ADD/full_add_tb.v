6499979babd4ba5da3767f69bd3f3c35e51cca09
Yaram Naveen Chandu <124757902+YaramNaveenChandu@users.noreply.github.com>
2026-02-25 18:31:19 +0530

    Update full_add_tb.v

--- BASIC_GATES/FULL_ADD/full_add_tb.v
+++ BASIC_GATES/FULL_ADD/full_add_tb.v
@@ -1,1 +1,35 @@
-
+module full_add_tb;
+  
+  reg a,b,c;
+  
+  wire sum,carry;
+  
+  full_add dut (.a(a), .b(b), .c(c), .sum(sum), .carry(carry));
+  
+  initial
+    
+    begin
+      
+      $monitor($time,"a=%d, b=%d, c=%d, sum=%d, carry=%d",a,b,c,sum,carry);
+      
+      #5 a=0; b=0; c=0;
+      #5 a=0; b=0; c=1;
+      #5 a=0; b=1; c=0;
+      #5 a=0; b=1; c=1;
+      #5 a=1; b=0; c=0;
+      #5 a=1; b=0; c=1;
+      #5 a=1; b=1; c=0;
+      #5 a=1; b=1; c=1;
+      #20 $finish;
+      
+    end
+  initial
+    
+    begin
+      
+      $dumpfile("dump.vcd");
+      $dumpvars(3, full_add_tb);
+      
+    end
+  
+endmodule
