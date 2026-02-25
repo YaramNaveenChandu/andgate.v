6aab25c1bb62e421618fce0a18fd07db8cdc27d8
Yaram Naveen Chandu <124757902+YaramNaveenChandu@users.noreply.github.com>
2026-02-25 19:05:40 +0530

    Update half_add_tb.v

--- BASIC_GATES/HALF_ADD/half_add_tb.v
+++ BASIC_GATES/HALF_ADD/half_add_tb.v
@@ -1,1 +1,32 @@
-
+module half_add_tb;
+  
+  reg a,b;
+  
+  wire sum,carry;
+  
+  half_add dut (.a(a), .b(b), .sum(sum), .carry(carry));
+  
+  initial
+    
+    begin
+      
+      $monitor($time,"a=%d, b=%d, sum=%d, carry=%d, ",a,b,sum,carry);
+      
+      #5 a=0; b=0; 
+      #5 a=0; b=1; 
+      #5 a=1; b=0; 
+      #5 a=1; b=1; 
+      #20 $finish;
+      
+    end
+  
+  initial
+    
+    begin
+      
+      $dumpfile("dump.vcd");
+      $dumpvars(3, half_add_tb);
+      
+    end
+  
+endmodule
