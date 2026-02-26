73fd5ea4b70d843c68149bd858daa9f66715587f
Yaram Naveen Chandu <124757902+YaramNaveenChandu@users.noreply.github.com>
2026-02-26 19:44:01 +0530

    Update and rename 2X4 decoder to 2X4 decoder.v

---
+++ COMBINATIONS/DECODER/2X4 decoder.v
@@ -0,0 +1,15 @@
+module decoder_2x4 (a,b,y0,y1,y2,y3);
+  
+  input a,b;
+  
+  output y0, y1, y2, y3;
+  
+  assign y0 = ~a&~b;
+  
+  assign y1 = ~a&b;
+
+  assign y2 = a&~b;
+  
+  assign y3 = a&b;
+
+endmodule
